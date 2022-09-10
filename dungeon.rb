3b977fb201cf29ae529fcf217db071ba0e2a880f
Shane Phillips <shane@loongphillips.com>
2022-09-10 15:44:34 -0400

    define and connect 6 rooms, add inital hp & points

--- dungeon.rb
+++ dungeon.rb
@@ -3,12 +3,17 @@
 # points tracking
 # Backtracking? Or, "the door seals shut behind you."
 
+points = 0
+hp = 100
+
 puts "You are a brave adventurer, here on an adventure. You have entered the ancient, maze-like Doom Fortress, and must find your way to the exit."
+puts "HP: #{hp}"
+puts "Points: #{points}"
 
 def room1
-    puts "You enter the Doom Fortress and the door seals shut behind you. There is nothing here but dust and bones."
+    puts "You have entered the Doom Fortress, and the entrance has sealed shut behind you. There is nothing in this room but dust and a few scattered bones."
     puts "Three passages spread out before you. You must choose one."
-    puts "(Type only the letter, A, B or C)"
+    puts "(Type only the letter: A, B or C)"
     puts "  A.  Take the West passage."
     puts "  B.  Take the East passage."
     puts "  C.  Take the North passage."
@@ -25,58 +30,76 @@ def room1
   end
 
   def room2
-	puts "This cave has some bones on the floor.  There are 2 exits. You:"
-	puts "A.  Take the first exit."
-	puts "B.  Take the second exit."
+	puts "Rats and bugs scurry back to their hiding places as you enter. The floor of this room is covered in bones, and smells of blood."
+	puts "There is only the North passage available, otherwise you may return to the first room."
+	puts "A.  Return to the entrance."
+	puts "B.  Take the North passage."
 	answer = gets.chomp
 	if answer.downcase == "a"
 		@room = 1
 	elsif answer.downcase == "b"
-		@room = 5
+		@room = 4
 	else
-		puts "that is not one of the choices."
+		puts "Try again."
 	end
 end
 
   def room3
-	puts "This cave has some bones on the floor.  There are 2 exits. You:"
-	puts "A.  Take the first exit."
-	puts "B.  Take the second exit."
+	puts "This room is unusually quiet. The dust stirrs mysteriously."
+	puts "There is only the North passage available, otherwise you may return to the first room."
+	puts "A.  Return to the entrance."
+	puts "B.  Take the North passage."
 	answer = gets.chomp
 	if answer.downcase == "a"
 		@room = 1
 	elsif answer.downcase == "b"
-		@room = 5
+		@room = 6
 	else
-		puts "that is not one of the choices."
+		puts "Try again."
 	end
 end
 
   def room4
-	puts "This cave has some bones on the floor.  There are 2 exits. You:"
-	puts "A.  Take the first exit."
-	puts "B.  Take the second exit."
+	puts "A bloodthirsty minotaur inhabits this room! After a brutal battle, you emerge victorious..."
+	puts "You may continue North once more, or take the East passage, or return to the previous room."
+	puts "Be warned: should you return to this room, you may find a new minotaur has taken the last one's place!"
+	puts "HP: #{hp}"
+	puts "Points: #{points}"
+	puts "A.  Take the North passage."
+	puts "B.  Take the East passage."
+	puts "C.  Return to the previous room."
+	hp -= 50
+	points += 25
 	answer = gets.chomp
 	if answer.downcase == "a"
-		@room = 1
+		@room = 7
 	elsif answer.downcase == "b"
 		@room = 5
+	elsif answer.downcase == "c"
+		@room = 2
 	else
-		puts "that is not one of the choices."
+		puts "Try again."
 	end
 end
 
   def room5
-	puts "This cave has some bones on the floor.  There are 2 exits. You:"
-	puts "A.  Take the first exit."
-	puts "B.  Take the second exit."
+	puts "This room is wide, open and circular in shape. Statues of ancient gods are arrayed around the perimeter."
+	puts "This is the central chamber. There are passages to the North, South, East and West."
+	puts "A.  Take the North passage."
+	puts "B.  Take the East passage."
+	puts "C.  Take the West passage."
+	puts "D.  Return to the entrance."
 	answer = gets.chomp
 	if answer.downcase == "a"
-		@room = 1
+		@room = 8
 	elsif answer.downcase == "b"
-		@room = 5
+		@room = 6
+	elsif answer.downcase == "c"
+		@room = 4
+	elsif answer.downcase == "d"
+		@room = 1
 	else
-		puts "that is not one of the choices."
+		puts "Try again."
 	end
 end
 
